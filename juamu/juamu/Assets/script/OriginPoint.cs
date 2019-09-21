604fdaa4cf4b6e3123fd61f6f5ed61555b3ec56a
Gucci0109721 <55590322+Gucci0109721@users.noreply.github.com>
2019-09-21 16:56:02 +0900

    Commit

---
+++ juamu/juamu/Assets/script/OriginPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OriginPoint : MonoBehaviour
+{
+    public Camera maincamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
---
+++ juamu/juamu/Assets/script/PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float movex = 0f;
+    float movey = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movex = Input.GetAxis("Horizontal")*3;
+        movey = Input.GetAxis("Vertical")*3;
+        //movex=-1.0f;
+    }
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(movex, movey);          //移動
+    }
+}
